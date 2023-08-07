a6b39ea400f37c63c7496e076ad649dd76c2feb4
Pavel Vařenka <pvarenka@gwi.com>
2023-08-07 13:30:15 +0200

    fix parsing and subparsers

--- src/kubernetes_parser_nodes.py
+++ src/kubernetes_parser_nodes.py
@@ -11,13 +11,15 @@ from auth import AuthNodes
 from prettytable import PrettyTable
 from googleapiclient.discovery import build
 
+class UltimateHelpFormatter(
+    argparse.RawTextHelpFormatter, argparse.ArgumentDefaultsHelpFormatter
+):
+    pass
+
 class NodeList:
-    def __init__(self, context=False, cluster=None, project_id=None, zone=None, auth=[]):
+    def __init__(self, context=False, auth=[]):
         self.auth = auth
         self.context = context
-        self.cluster = cluster
-        self.project_id = project_id
-        self.zone = zone
 
     def list_nodes(self):
         if self.context is None:
@@ -49,6 +51,13 @@ class NodeList:
 
         else:
             print("No nodes in context, please choose a different one.")
+    
+class PoolList:
+    def __init__(self, cluster=None, project_id=None, zone=None, auth=[]):
+        self.auth = auth
+        self.cluster = cluster
+        self.project_id = project_id
+        self.zone = zone
 
     def list_nodes_in_pool(self):
         """Lists all clusters and associated node pools."""
@@ -58,13 +67,13 @@ class NodeList:
         clusters_response = clusters_resource.list(
             projectId=self.project_id, zone=self.zone
         ).execute()
-        # t = PrettyTable()
-        # names_cluster = []
-        # status_cluster = []
-        # version_cluster = []
-        # names_nodepool = []
-        # status_nodepool = []
-        # config_nodepool = []
+        t = PrettyTable()
+        names_cluster = []
+        status_cluster = []
+        version_cluster = []
+        names_nodepool = []
+        status_nodepool = []
+        config_nodepool = []
 
         for cluster in clusters_response.get("clusters", []):
             print(
@@ -72,11 +81,14 @@ class NodeList:
                     cluster["name"], cluster["status"], cluster["currentMasterVersion"]
                 )
             )
-            # t.add_column('Cluster', cluster["name"])
-            # t.add_column('Cluster status', cluster["status"])
-            # names_cluster.append(cluster["name"])
-            # status_cluster.append(cluster["status"])
-            # version_cluster.append(cluster["currentMasterVersion"])
+
+            # t.add_column('Cluster name', names_cluster)
+            names_cluster.append(cluster["name"])
+            status_cluster.append(cluster["status"])
+            version_cluster.append(cluster["currentMasterVersion"]) 
+            t.add_column('Cluster', names_cluster)
+            t.add_column('Cluster status', status_cluster)
+            t.add_column('Cluster version', version_cluster)
 
             nodepools_response = (
                 clusters_resource.nodePools()
@@ -87,7 +99,7 @@ class NodeList:
             # t.add_column('Cluster name', names_cluster)
             # t.add_column('Cluster status', status_cluster)
             # t.add_column('Version', version_cluster)
-            # print(t)
+            print(t)
 
             for nodepool in nodepools_response["nodePools"]:
                 print(
@@ -107,51 +119,104 @@ class NodeList:
 if __name__ == '__main__':
     try:
         """Command-line prompts."""
-        parser = argparse.ArgumentParser(prog='parse_nodes',
-                                        usage='nodes [context]',
+        parser = argparse.ArgumentParser(prog='Kubernetes parser',
+                                        usage='nodes [command]',
                                         description=__doc__,
-                                        formatter_class=argparse.RawDescriptionHelpFormatter)
-        parser.add_argument("--context",
+                                        formatter_class=UltimateHelpFormatter)
+        subparsers = parser.add_subparsers(dest='command')
+        subparsers.required = True
+
+        # List-nodes command
+        lnodes = subparsers.add_parser('list_nodes', help='List nodes in context.', formatter_class=UltimateHelpFormatter)
+        lnodes.add_argument("--context", "-c",
             help='The context for kubernetes cluster.',
             action='store',
-            default=None,
+            default=None,   
             dest="context",
             # nargs='?'
         )
-        parser.add_argument("--cluster",
-            help='Cluster to list the nodes in.',
-            action='store',
-            default=None,
-            dest="cluster",
-            nargs='?'
-        )
-        parser.add_argument("--project_id",
-            help='Project ID instead of context.',
-            action='store',
-            default=None,
-            dest="project_id",
-            nargs='?'
-        )
-        parser.add_argument("--zone",
-            help='Zone to list nodepools in. Serves as a replacement for --context in combination with --project_id',
-            action='store',
-            default=None,
-            dest="zone",
-            nargs='?'
-        )
-        parser.add_argument("--limit",
+        # lnodes.add_argument("--cluster",
+        #     help='Cluster to list the nodes in.',
+        #     action='store',
+        #     default=None,
+        #     dest="cluster",
+        #     nargs='?'
+        # )
+        # lnodes.add_argument("--project_id",
+        #     help='Project ID instead of context.',
+        #     action='store',
+        #     default=None,
+        #     dest="project_id",
+        #     nargs='?'
+        # )
+        # lnodes.add_argument("--zone",
+        #     help='Zone to list nodepools in. Serves as a replacement for --context in combination with --project_id',
+        #     action='store',
+        #     default=None,
+        #     dest="zone",
+        #     nargs='?'
+        # )
+        lnodes.add_argument("--limit",
             help='Maximum number of pods to list.',
             dest="limit",
             required=False
         )
-        parser.add_argument("--timeout_seconds",
+        lnodes.add_argument("--timeout_seconds",
             help='Timeout in seconds.',
             dest="timeout_seconds",
             nargs="?",
             type=int,
             required=False
         )
-        parser.add_argument("--watch",
+        lnodes.add_argument("--watch",
+            help='True or false... watch.',
+            action='store_true',
+            required=False
+        )
+
+        # List-nodepools command
+        lpools = subparsers.add_parser('list_nodepools', help='List nodepools and clusters in project and zone.', formatter_class=UltimateHelpFormatter)
+        lpools.add_argument("--cluster", "-c",
+            help='Cluster to list the nodes in.',
+            action='store',
+            default=None,
+            dest="cluster",
+            nargs='?'
+        )
+        # lpools.add_argument("--context",
+        #     help='The context for kubernetes cluster.',
+        #     action='store',
+        #     default=None,
+        #     dest="context",
+        #     # nargs='?'
+        # )
+        lpools.add_argument("--project_id", "-p",
+            help='Project ID instead of context.',
+            action='store',
+            default=None,
+            dest="project_id",
+            nargs='?'
+        )
+        lpools.add_argument("--zone", "-z",
+            help='Zone to list nodepools in. Serves as a replacement for --context in combination with --project_id',
+            action='store',
+            default=None,
+            dest="zone",
+            nargs='?'
+        )
+        lpools.add_argument("--limit", "-l",
+            help='Maximum number of pods to list.',
+            dest="limit",
+            required=False
+        )
+        lpools.add_argument("--timeout_seconds", "-t",
+            help='Timeout in seconds.',
+            dest="timeout_seconds",
+            nargs="?",
+            type=int,
+            required=False
+        )
+        lpools.add_argument("--watch", "-w",
             help='True or false... watch.',
             action='store_true',
             required=False
@@ -162,20 +227,29 @@ if __name__ == '__main__':
         # if arguments.context is None:
         #     parser.error("'Context' is required.")
 
-        auth = AuthNodes(auth_method='local', context=arguments.context)
+        if arguments.command == 'list_nodes':
+            # if all(item is None for item in [arguments.cluster, arguments.project_id, arguments.zone]) and arguments.context is not None:
+            if arguments.context is None:
+                sys.tracebacklimit = 0
+                raise Exception("Please specify context.")
 
-        if all(item is None for item in [arguments.cluster, arguments.project_id, arguments.zone]) and arguments.context is not None:
-            list_nodes = NodeList(arguments.context, arguments.cluster, arguments.project_id, arguments.zone, auth)
+            auth = AuthNodes(auth_method='local', context=arguments.context)
+            list_nodes = NodeList(arguments.context, auth)
             list_nodes.list_nodes()
 
-        elif all(item is not None for item in [arguments.cluster, arguments.project_id, arguments.zone]) and arguments.context is None:
-            list_nodes = NodeList(arguments.context, arguments.cluster, arguments.project_id, arguments.zone, auth)
-            list_nodes.list_nodes_in_pool()
+        elif arguments.command == 'list_nodepools':
+            # elif all(item is not None for item in [arguments.cluster, arguments.project_id, arguments.zone]) and arguments.context is None:
+            if any(item is None for item in [arguments.project_id, arguments.zone, arguments.cluster]):
+                sys.tracebacklimit = 0
+                raise Exception("Please specify project_id, zone and cluster.")
+            
+            list_nodepools = PoolList(arguments.cluster, arguments.project_id, arguments.zone)
+            list_nodepools.list_nodes_in_pool()
 
-        else:
+        elif arguments.command is None:
             sys.tracebacklimit = 0
-            raise Exception("Context, project ID or zone is missing.\nTo list nodepools and clusters, specify project, zone and cluster. To list nodes, specify context.")
-            parser.print_help()
+            raise Exception("Please specify command: 1) list_nodes, 2) list_nodepools")
+            lpools.print_help()
 
     except KeyboardInterrupt:
         print('Aborted.')
